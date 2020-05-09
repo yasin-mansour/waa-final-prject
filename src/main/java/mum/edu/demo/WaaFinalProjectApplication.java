935fb5d8f8802f42815c4151505636fa91d0ab51
Yasin Mansour <yasenmansor@yahoo.com>
2020-05-08 21:30:32 -0500

    first commit

---
+++ src/main/java/mum/edu/demo/WaaFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package mum.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaaFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaaFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mum/edu/demo/WaaFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mum.edu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaaFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
